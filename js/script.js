d65cdbadbdc94ad0b44b941eab04997d17ce91c3
wanderley <camposwanderley418@gmail.com>
2024-06-24 16:03:35 -0300

    troca de animação na particles/ filter drop-shadow

--- js/script.js
+++ js/script.js
@@ -1,68 +1,63 @@
-const btnUp = document.querySelector('#btn-up')
+const btnUp = document.querySelector("#btn-up");
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.header-do-read', { duration: 2000, reset: false });
+sr.reveal(".header-do-read", { duration: 2000, reset: false });
 
-sr.reveal('.head-container', {
-    duration: 2000,
-    delay: 200,
-    distance: '80px',
-    origin: 'top',
-    reset: false
+sr.reveal(".head-container", {
+  duration: 2000,
+  delay: 200,
+  distance: "80px",
+  reset: false,
 });
 
-sr.reveal('.title', { duration: 2000, reset:false });
+sr.reveal(".title", { duration: 2000, reset: false });
 
-sr.reveal('.texto-sobre', { duration: 2000,
-origin:'left',
-distance:'50px',
-reset:false
+sr.reveal(".texto-sobre", {
+  duration: 2000,
+  origin: "left",
+  distance: "50px",
+  reset: false,
 });
 
-sr.reveal('.sobre-image', { 
-    duration: 2000,
-    delay:200,
-    origin:'top',
-    distance:'50px',
-    reset:false
+sr.reveal(".sobre-image", {
+  duration: 2000,
+  delay: 200,
+  origin: "top",
+  distance: "50px",
+  reset: false,
 });
 
-sr.reveal('.card-projetos', { 
-    duration: 2000,
-    delay:200,
-    reset:false
+sr.reveal(".card-projetos", {
+  duration: 2000,
+  delay: 200,
+  reset: false,
 });
 
-sr.reveal('.card-icons', { 
-    duration: 2000,
-    delay:200,
-    distance: '80px',
-    origin: 'top',
-    rotate: { x: 0, y: 80, z: 0},
-    reset:false
-
+sr.reveal(".card-icons", {
+  duration: 2000,
+  delay: 200,
+  distance: "80px",
+  origin: "top",
+  rotate: { x: 0, y: 80, z: 0 },
+  reset: false,
 });
 
-
-
-
-
-btnUp.addEventListener('click', () => {
-    window.scrollTo(0, 0)
-    document.removeEventListener('scroll', ocult)
-})
+btnUp.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+  document.removeEventListener("scroll", ocult);
+});
 
 const ocult = () => {
-    if (window.scrollY > 10) {
-        btnUp.style.display = 'flex'
-    } else {
-        btnUp.style.display = 'none'
-    }
-}
+  if (window.scrollY > 10) {
+    btnUp.style.display = "flex";
+  } else {
+    btnUp.style.display = "none";
+  }
+};
 
-document.addEventListener('scroll', () => {
-    ocult()
-    document.removeEventListener('scroll', ocult)
-})
+document.addEventListener("scroll", () => {
+  ocult();
+  document.removeEventListener("scroll", ocult);
+});
 
-ocult()
+ocult();
